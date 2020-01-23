f57bb15778d5fc212a18066e26be29a6c405bd58
lizhen <uowyuchen@gmail.com>
2020-01-23 12:29:29 -0500

    YelpCamp V2 - Data Persistence, RESTful Routes, method-override, express-sanitizer, Data Associations and moduel.export

--- app.js
+++ app.js
@@ -1,69 +1,91 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-const campgrounds = [
-  {
-    name: "Salmon Creek",
-    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"
-  }
-];
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+// Model Setup
+const Camground = mongoose.model("Campground", campgroundSchema);
+// Create Campground
+// Camground.create(
+//   {
+//     name: "Mountain Goat's Rest",
+//     image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg",
+//     description: "This is a huge granite hill, no bathrooms, No Water!"
+//   },
+//   (err, campground) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("New Created Campground");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
+// Index - show all campgrounds
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  // Get All Campgrounds
+  Camground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allCampgrounds });
+    }
+  });
 });
 
+// Create - add new campground to DB
 app.post("/campgrounds", (req, res) => {
   // get data from form and add to campgrounds array
   const name = req.body.name;
   const image = req.body.image;
-  const newCampgrounds = { name, image };
-  campgrounds.unshift(newCampgrounds);
-  // redirect back to campgrounds page
-  res.redirect("/campgrounds");
+  const description = req.body.description;
+
+  const newCampgrounds = { name, image, description };
+  // Create a new campground and save to DB
+  Camground.create(newCampgrounds, (err, newCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
+// New - show form to create new campground
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
+});
+
+// Show - show more info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+  // find the campground with provided ID
+  Camground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render show template with that campground
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
