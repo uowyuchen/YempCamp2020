5261175d179f6c3d78d519875ec005d833ef911e
lizhen <uowyuchen@gmail.com>
2020-01-23 12:31:08 -0500

    YelpCamp V3 - Separate model file and seeding database

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  mongoose = require("mongoose");
+  mongoose = require("mongoose"),
+  Camground = require("./models/campground"),
+  seedDB = require("./seed");
 
+seedDB();
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
-// Schema Setup
-const campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-// Model Setup
-const Camground = mongoose.model("Campground", campgroundSchema);
-// Create Campground
-// Camground.create(
-//   {
-//     name: "Mountain Goat's Rest",
-//     image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg",
-//     description: "This is a huge granite hill, no bathrooms, No Water!"
-//   },
-//   (err, campground) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("New Created Campground");
-//       console.log(campground);
-//     }
-//   }
-// );
 
 app.get("/", (req, res) => {
   res.render("landing");
@@ -78,14 +56,16 @@ app.get("/campgrounds/new", (req, res) => {
 // Show - show more info about one campground
 app.get("/campgrounds/:id", (req, res) => {
   // find the campground with provided ID
-  Camground.findById(req.params.id, (err, foundCampground) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // render show template with that campground
-      res.render("show", { campground: foundCampground });
-    }
-  });
+  Camground.findById(req.params.id)
+    .populate("comments")
+    .exec((err, foundCampground) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // render show template with that campground
+        res.render("show", { campground: foundCampground });
+      }
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
---
+++ models/campground.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+// Model Setup
+module.exports = mongoose.model("Campground", campgroundSchema);
